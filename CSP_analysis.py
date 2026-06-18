d539f2b848176ebc7289437524e65951d239e93a
Jake Anderson <jake.t.anderson@icloud.com>
2026-06-18 15:04:04 -0400

    Added autoscaling and image formats w/ metadata

--- CSP_analysis.py
+++ CSP_analysis.py
@@ -1,11 +1,13 @@
 import argparse
-import pandas as pd
-import numpy as np
 import logging
-import sys
-import matplotlib.pyplot as plt
-from datetime import datetime
 import os
+import sys
+from datetime import datetime
+
+import colorlog
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 
 # Chemical shift perturbations, from Eq 8 of doi.org/10.1016/j.pnmrs.2013.02.001
@@ -15,11 +17,22 @@ def CSP(w1: float, w2: float, alpha1: float, alpha2: float):
 
 def setup_logging(no_log: bool, log: str):
     logger = logging.getLogger()
-    logger.setLevel(logging.WARNING if no_log else logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s- %(levelname)s - %(message)s")
 
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setFormatter(formatter)
+    stdout_handler = colorlog.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(
+        colorlog.ColoredFormatter(
+            "%(log_color)s%(levelname)s%(reset)s - %(message)s",
+            log_colors={
+                "DEBUG": "cyan",
+                "INFO": "green",
+                "WARNING": "orange",
+                "ERROR": "red",
+            },
+        )
+    )
+
     logger.addHandler(stdout_handler)
 
     if not no_log:
@@ -30,22 +43,26 @@ def setup_logging(no_log: bool, log: str):
     return logger
 
 
-def get_consistent_max_residue_index(peaklists: List[pd.DataFrame], logger):
+def get_consistent_max_residue(peaklists: List[pd.DataFrame], logger) -> (int, float):
     max_residue_indices = []
+    max_vals = []
     for peaklist in peaklists[1:]:
         max_residue_indices.append(peaklist["CSP"].idxmax())
+        max_vals.append(peaklist["CSP"].max())
 
     max_residue_indices = set(max_residue_indices)
     if len(max_residue_indices) == 1:
         logger.info(
             f"Residue {peaklist.loc[list(max_residue_indices)[0]]['Assignment']} (index {list(max_residue_indices)[0]}) has the maximum CSP for all titrants."
         )
-        return list(max_residue_indices)[0]
+        index = list(max_residue_indices)[0]
+        value = max(max_vals)
+        return index, value
     else:
         logger.info(
             "The residue with the maximum CSP is different across the titration. No autoscaling possible"
         )
-        return None
+        return None, None
 
 
 def main():
@@ -61,6 +78,12 @@ def main():
         help='A list of residues for which the peaks are bad and ought to be excluded from analysis. E.g. "9,15,20".',
     )
     parser.add_argument(
+        "--residue-offset",
+        type=int,
+        default=0,
+        help="The starting index will be shifted by this amount.",
+    )
+    parser.add_argument(
         "--log",
         type=str,
         default="CSP_analysis.log",
@@ -74,10 +97,10 @@ def main():
         help="The directory to which you'd like to save logs and plots from this script.",
     )
     parser.add_argument(
-        "--residue-offset",
-        type=int,
-        default=0,
-        help="The starting index will be shifted by this amount."
+        "--plot-format",
+        type=str,
+        default="pdf",
+        help='The file format you\'d like the plots to be given in. Valid options are "png", "svg", and "pdf".',
     )
     args = parser.parse_args()
 
@@ -86,8 +109,15 @@ def main():
         os.mkdir(args.dir)
     else:
         raise AssertionError(f"The analysis directory {args.dir} already exists!")
-    logger = setup_logging(args.no_log, f"{args.dir}/{args.log}")
-    logging.info("You ran: " + str(sys.executable) + " " + " ".join(sys.argv))
+    logfile = os.path.abspath(f"{args.dir}/{args.log}")
+    logger = setup_logging(args.no_log, logfile)
+    logger.info("You ran: " + str(sys.executable) + " " + os.path.abspath(__file__))
+    logger.info(f"This log file is located at {logfile}")
+
+    # Check that the filetype is valid
+    if args.plot_format not in ("png", "svg", "pdf"):
+        logger.error(f'File format "{args.plot_format}" is invalid!')
+        raise AssertionError(f'File format "{args.plot_format}" is invalid!')
 
     badresidues = [
         badres for badres in args.bad_residues.split(",") if badres.isdigit()
@@ -104,8 +134,10 @@ def main():
     # Remove the "done" since it's not a peakfile
     peakfiles.pop(-1)
     # Remove any peakfiles that were from just pressing enter
-    peakfiles = [peakfile for peakfile in peakfiles if peakfile.strip() != ""]
-    logging.info(f"Using peakfiles:\n  {'\n  '.join(peakfiles)}")
+    peakfiles = [
+        os.path.abspath(peakfile) for peakfile in peakfiles if peakfile.strip() != ""
+    ]
+    logger.info(f"Using peakfiles:\n  {'\n  '.join(peakfiles)}")
 
     titration_percs = []
     peaklists = []
@@ -146,6 +178,7 @@ def main():
         logger.warning(
             "The lowest titration percentage is not zero. Did you make a mistake? It will still be used as a reference... be careful!"
         )
+    logger.info(f"Using titration percentages {titration_percs}")
 
     assert (
         len(set([len(peaklist) for peaklist in peaklists])) == 1
@@ -161,9 +194,11 @@ def main():
     alphas = {"H": 1.0}
     for nucleus in unique_nonH_nuclei:
         alphas[nucleus] = float(
-            input(f"Give the alpha for scaling perturbations in {nucleus}: ").strip()
+            input(
+                f"Give the alpha for scaling perturbations in {nucleus} relative to H: "
+            ).strip()
         )
-        alphas[nucleus] = 0.2
+    logger.info(f"Using alphas: {alphas}")
 
     for peaklist in peaklists:
         # We are comparing this peaklist to the first peaklist, which has the lowest titration value
@@ -190,12 +225,12 @@ def main():
         )
 
     # Perform autoscaling to a consistent residue's maximum CSP
-    max_residue_index = get_consistent_max_residue_index(peaklists, logger)
+    max_residue_index, max_value = get_consistent_max_residue(peaklists, logger)
     if max_residue_index:
         while True:
             autoscale = (
                 input(
-                    "Would you like to autoscale this residue's CSP to 1 in each histogram? (y/n): "
+                    "Would you like to autoscale so this residue's maximum is at the top of each plot? (y/n): "
                 )
                 .lower()
                 .strip()
@@ -207,12 +242,18 @@ def main():
                 autoscale = False
                 break
 
-    plt.rcParams.update({'font.size': 14})
+    logger.info(f"Creating {args.plot_format.upper()}s")
+    # Include the full log in the metadata of the image
+    with open(f"{args.dir}/{args.log}", "r") as f:
+        full_log = f.read()
+    plt.rcParams.update({"font.size": 14})
     for nucleus in unique_nonH_nuclei:
-        for i, peaklist in enumerate(peaklists[1:]): # We do [1:] because first peaklist is baseline and the CSPs=0
-            fig = plt.figure(figsize=(16,9))
-            plt.title(f"H{nucleus} at {titration_percs[i]}%")
-            peaks_filtered = peaklist[(peaklist['Nucleus1'] == nucleus)]
+        for i, peaklist in enumerate(
+            peaklists[1:]
+        ):  # We do [1:] because first peaklist is baseline and the CSPs=0
+            fig = plt.figure(figsize=(16, 9))
+            plt.title(f"H{nucleus} at {titration_percs[i+1]}%")
+            peaks_filtered = peaklist[(peaklist["Nucleus1"] == nucleus)]
             CSPs_np = peaks_filtered.CSP.to_numpy()
             mean = np.mean(CSPs_np)
             median = np.median(CSPs_np)
@@ -225,17 +266,72 @@ def main():
                 else:
                     colors.append("blue")
 
-            plt.bar(peaks_filtered.ResidueIndex.to_numpy(dtype=int), CSPs_np, color=colors, width=1.0)
-            plt.axhline(y=mean, color="black", linestyle='-', label=f"Mean: {mean:0.3f}")
-            plt.axhline(y=mean+stdev, color="black", linestyle='--', label=f"Stdev: {stdev:0.3f}")
-            plt.axhline(y=mean-stdev, color="black", linestyle='--',)
-            plt.xticks(peaks_filtered.ResidueIndex.to_numpy(dtype=int), labels=peaks_filtered["Assignment"].str.split("_").str[0].tolist(), fontsize=8, rotation=45)
-            plt.xlim(xmin=peaks_filtered.ResidueIndex.to_numpy(dtype=int).min()-0.5, xmax=peaks_filtered.ResidueIndex.to_numpy(dtype=int).max()+0.5)
+            plt.bar(
+                peaks_filtered.ResidueIndex.to_numpy(dtype=int),
+                CSPs_np,
+                color=colors,
+                width=1.0,
+            )
+            plt.axhline(
+                y=mean,
+                color="black",
+                linestyle="-",
+                label=r"$\overline{\Delta \delta}$",
+            )
+            plt.axhline(
+                y=mean + stdev,
+                color="black",
+                linestyle="--",
+                label=r"$\overline{\Delta \delta} \pm \sigma$",
+            )
+            plt.axhline(
+                y=mean - stdev,
+                color="black",
+                linestyle="--",
+            )
+
+            plt.scatter(
+                min(peaks_filtered.ResidueIndex.to_numpy(dtype=int)),
+                0,
+                label=" ",
+                visible=False,
+            )
+            plt.scatter(
+                min(peaks_filtered.ResidueIndex.to_numpy(dtype=int)),
+                0,
+                label=r"$\overline{\Delta \delta}$"
+                + f" = {mean:0.3f} ppm,\n"
+                + r"$\sigma$"
+                + f"   = {stdev:0.3f} ppm",
+                visible=False,
+            )
+
+            # For labeling the residues on the x axis. Maybe allow as an option later.
+            # plt.xticks(
+            #     peaks_filtered.ResidueIndex.to_numpy(dtype=int),
+            #     labels=peaks_filtered["Assignment"].str.split("_").str[0].tolist(),
+            #     fontsize=8,
+            #     rotation=45,
+            # )
+            plt.xticks(
+                np.arange(0, max(peaks_filtered.ResidueIndex.to_numpy(dtype=int)), 5),
+            )
+            plt.xlim(
+                xmin=peaks_filtered.ResidueIndex.to_numpy(dtype=int).min() - 0.5,
+                xmax=peaks_filtered.ResidueIndex.to_numpy(dtype=int).max() + 0.5,
+            )
             plt.xlabel("Residues")
             plt.ylabel(r"$\Delta \delta$" + " (ppm)")
-            plt.legend()
+
+            if autoscale:
+                plt.ylim(0, max_value)
+            plt.legend(framealpha=0.75)
             plt.tight_layout()
-            plt.savefig(f"{args.dir}/H{nucleus}_{titration_percs[i+1]}.png")
+
+            plt.savefig(
+                f"{args.dir}/H{nucleus}_{titration_percs[i+1]}.{args.plot_format}",
+                metadata={"Title": full_log},  # Title should work across all 3 formats
+            )
 
 
 if __name__ == "__main__":
